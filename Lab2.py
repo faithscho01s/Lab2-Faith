a45b78934bcdaac1d649ba4539259b516e1e0aa8
FaithDeVera <faithkirstendv14.21@ichat.sp.edu.sg>
2022-10-26 18:02:00 +0800

    Stuck at Exercise 4 7.4

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,35 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+    calc_average(num_list)
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():
+    x = input()
+    y = x.split(",")
+    print("get_user_input")
+    list = []
+    for x in y:
+        list.append(round(float(x), 1))
+    print(list)
+    return list
+
+def calc_average(list):
+    print("calc_average")
+    average = sum(list)/len(list)
+    return float(average)
+
+
+def find_min_max():
+    print("find_min_max")
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+
+if __name__ == "__main__":
+    main()
---
+++ bmi.py
@@ -0,0 +1,13 @@
+def calculate_bmi (height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = (weight/(height*height))
+    if bmi < 18.5:
+        print("Under Weight")
+    elif 18.5 <= bmi <= 25.0:
+        print("Normal Weight")
+    else:
+        print("Over Weight")
+
+calculate_bmi(weight=57, height=1.73)
