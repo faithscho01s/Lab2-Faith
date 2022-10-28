81e7a1dc863c2519a561186d96a9a23ddc0c89ee
FaithDeVera <faithkirstendv14.21@ichat.sp.edu.sg>
2022-10-28 18:17:38 +0800

    Exercise 4 is done

--- Lab2.py
+++ Lab2.py
@@ -2,34 +2,44 @@ def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     num_list = get_user_input()
-    calc_average(num_list)
+    print(num_list)
+    print(calc_average(num_list))
+    print(find_min_max(num_list))
+
+
 def display_main_menu():
+    print("display_main_menu")
     print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
 
+
 def get_user_input():
-    x = input()
-    y = x.split(",")
     print("get_user_input")
-    list = []
-    for x in y:
-        list.append(round(float(x), 1))
-    print(list)
-    return list
+    x = input()
+    y = x.split(',')
+    return y
 
-def calc_average(list):
+
+def calc_average(num_list):
     print("calc_average")
-    average = sum(list)/len(list)
-    return float(average)
+    num_list = list(map(int, num_list))
+    average = sum(num_list) / len(num_list)
+    return average
 
 
-def find_min_max():
+def find_min_max(num_list):
     print("find_min_max")
+    min = num_list[0]
+    for number in num_list:
+        if number > min:
+            min = number
 
-def sort_temperature():
-    print("sort_temperature")
+    max = num_list[0]
+    for number in num_list:
+        if number < max:
+            max = number
+    return min, max
 
-def calc_median_temperature():
-    print("calc_median_temperature")
+
 
 if __name__ == "__main__":
     main()
