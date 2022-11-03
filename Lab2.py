b70b6a6e31daa387152a6f158a7e5bf0852302a4
FaithDeVera <faithkirstendv14.21@ichat.sp.edu.sg>
2022-11-03 22:12:24 +0800

    added median temeperature

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,4 @@
+import math
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
@@ -5,11 +6,14 @@ def main():
     print(num_list)
     print(calc_average(num_list))
     print(find_min_max(num_list))
+    new_list = sort_temperature(num_list)
+    print(new_list)
+    print(calc_median_temperature(new_list))
 
 
 def display_main_menu():
     print("display_main_menu")
-    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+    print("Enter some numbers separated by commas (e.g. 5,67,32)")
 
 
 def get_user_input():
@@ -38,9 +42,31 @@ def find_min_max(num_list):
     for number in num_list:
         if number > max:
             max = number
-    return min, max
+    return [min, max]
 
+def sort_temperature(num_list):
+    print("sort_temperature")
+    num_list.sort()
+    return num_list
 
+def calc_median_temperature(new_list):
+    print("calc_median_temperature")
+    n = len(new_list)
+
+    if n % 2 != 0:
+        #odd
+        find_index_odd = n / 2
+        odd = math.floor(find_index_odd)
+        median = new_list[odd]
+    else:
+        #even
+        find_index_even = n / 2
+        even = math.ceil(find_index_even)
+        middle1 = int(new_list[even])
+        middle2 = int(new_list[even - 1])
+        median = (middle1 + middle2) / 2
+
+    return median
 
 if __name__ == "__main__":
     main()
--- Test_Lab2.py
+++ Test_Lab2.py
@@ -1,7 +1,12 @@
 import Lab2
 
-##def test_find_min_max():
+def test_find_min_max():
+    result = []
+    user_input = [34, 56, 78, 90]
+    test_min_max = [34, 90]
+    min_max = Lab2.find_min_max(user_input)
 
+    assert (min_max == test_min_max)
 
 def test_calc_average():
     result = []
